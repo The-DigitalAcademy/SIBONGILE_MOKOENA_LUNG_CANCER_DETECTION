7425d03ce661613d4f825147e7c83377c773be6a
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-08-18 11:03:35 +0200

    Add files via upload

--- UI.py
+++ UI.py
@@ -1,15 +1,22 @@
+import pandas as pd
 import streamlit as st
 import joblib
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 
 # Function to load the models
 def load_models():
     forest_model_path = '/Users/da_m1_18/Downloads/LUNGS/forest_model.pkl'
     logis_model_path = '/Users/da_m1_18/Downloads/LUNGS/logis_model.pkl'
+    gumodel_path = '/Users/da_m1_18/Downloads/LUNGS/gumodel.pkl'
+    dtree_model_path = '/Users/da_m1_18/Downloads/LUNGS/dtree.pkl'
     
     forest_model = joblib.load(forest_model_path)    
     logis_model = joblib.load(logis_model_path)
+    gumodel = joblib.load(gumodel_path)
+    dtree_model = joblib.load(dtree_model_path)
     
-    return logis_model, forest_model
+    return logis_model, forest_model, gumodel, dtree_model
 
 def main():
     # Title of the web app
@@ -17,19 +24,22 @@ def main():
     st.write("Enter the following features to get predictions:")
 
     # Load the models
-    logis_model, forest_model = load_models()
+    logis_model, forest_model, gumodel, dtree_model = load_models()
 
     # User input for features
     st.header('Feature Input')
 
     # Create input fields for each feature
     # For numerical features, use st.number_input()
-    feature1 = st.number_input("SMOKING", value =0)
-    feature2 = st.number_input("PEER_PRESSURE", value = 0)
-    feature3 = st.number_input("ALCOHOL CONSUMING", value = 0)
+    feature1 = st.number_input("SMOKING", value=0)
+    feature2 = st.number_input("PEER_PRESSURE", value=0)
+    feature3 = st.number_input("ALCOHOL CONSUMING", value=0)
     feature4 = st.number_input("CHEST PAIN", value=0)
+    feature5 = st.number_input("CHRONIC DISEASE", value=0)
+   
+    
     # Selection box for the model to use
-    selected_model = st.selectbox("Choose a model", ["Logistic Regression", "Random Forest"])
+    selected_model = st.selectbox("Choose a model", ["Logistic Regression", "Random Forest", "Gaussian Naive Bayes", "Decision Tree"])
 
     # Button for predictions
     clicked = st.button('Get Predictions')
@@ -38,11 +48,22 @@ def main():
     if clicked:
         if selected_model == "Logistic Regression":
             model = logis_model
-        else:
+        elif selected_model == "Random Forest":
             model = forest_model
+        elif selected_model == "Gaussian Naive Bayes":
+            model = gumodel
+        else:
+            model = dtree_model
 
         # Perform predictions using the selected model
-        prediction = model.predict([[feature1, feature2, feature3, feature4]])
+        input_features = pd.DataFrame({
+            "SMOKING":[feature1],
+            "PEER_PRESSURE": [feature2],
+            "ALCOHOL CONSUMING": [feature3],
+            "CHEST PAIN": [feature4],
+            "CHRONIC DISEASE":[feature5]
+        })
+        prediction = model.predict(input_features)
 
         # Display the prediction result
         st.header('Prediction')
@@ -50,4 +71,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
